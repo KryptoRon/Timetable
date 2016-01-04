7ecc54b65e4a34d7766188dfb24d1ee0416727fe
KryptoRon <ronald.huizer@gmail.com>
2016-01-04 13:56:33 +0100

    Parse SignUp and Login

--- app/src/main/java/com/kb5012/timetable/LoginActivity.java
+++ app/src/main/java/com/kb5012/timetable/LoginActivity.java
@@ -7,33 +7,96 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 /**
  * Created by Ronald on 14-12-2015.
  */
 public class LoginActivity extends AppCompatActivity {
+
+    String usernametxt;
+    String passwordtxt;
+    EditText password;
+    EditText username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Locate EditTexts in main.xml
+        username = (EditText) findViewById(R.id.tf_username);
+        password = (EditText) findViewById(R.id.tf_password);
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/android/guide#local-datastore
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this);
     }
 
-    public void onclick(View v) {
-        String username = "" + ((EditText) findViewById(R.id.tf_username)).getText();
-        String password = "" + ((EditText) findViewById(R.id.tf_password)).getText();
+    public void signIn(View v) {
+        usernametxt = username.getText().toString();
+        passwordtxt = password.getText().toString();
 
-        User user = DBHelper.userInlog(username, password);
+        ParseUser.logInInBackground(usernametxt, passwordtxt,
+                new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            // If user exist and authenticated, send user to Welcome.class
+                            Intent intent = new Intent(getApplicationContext(), UserScreen.class);
+                            Bundle b = new Bundle();
+                            b.putString("UserID" , user.getObjectId());
+                            intent.putExtras(b);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(),
+                                    "Successfully Logged in",
+                                    Toast.LENGTH_LONG).show();
+                            finish();
+                        } else {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "No such user exist, please signup",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 
-        if (user == null) {
-            Toast.makeText(getApplicationContext(), "Login failled, try again", Toast.LENGTH_LONG).show();
+    public void signUp(View v) {
+        usernametxt = username.getText().toString();
+        passwordtxt = password.getText().toString();
+        // Force user to fill up the form
+        if (usernametxt.equals("") && passwordtxt.equals("")) {
+            Toast.makeText(getApplicationContext(),
+                    "Please complete the sign up form",
+                    Toast.LENGTH_LONG).show();
+
         } else {
-            Intent intent = new Intent(getApplicationContext(), UserScreen.class);
-            Bundle b = new Bundle();
-            b.putInt("userID", user.getId());
-            intent.putExtras(b);
-            startActivity(intent);
-            finish();
+            // Save new user data into Parse.com Data Storage
+            ParseUser user = new ParseUser();
+            user.setUsername(usernametxt);
+            user.setPassword(passwordtxt);
+            user.signUpInBackground(new SignUpCallback() {
+                public void done(ParseException e) {
+                    if (e == null) {
+                        // Show a simple Toast message upon successful registration
+                        Toast.makeText(getApplicationContext(),
+                                "Successfully Signed up, please log in.",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "Sign up Error", Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }
+            });
         }
     }
+
     public void test (View v){
         Intent intent = new Intent(getApplicationContext(), UserScreen.class);
         Bundle b = new Bundle();
